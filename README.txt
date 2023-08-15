d93a2ee4afc2dd421e192e825259cc25017732f1
Yogesh Kumar <138758943+yogku@users.noreply.github.com>
2023-08-15 21:44:58 +0530

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
 Hii!, I am yogesh.
-  
+
 this is my first project Memory Lane.
--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-const sidebar = document.querySelector('.sidebar');
-const sidebarButton = document.querySelector('.sidebarButton');
 const profileIcon = document.querySelector('.profileIcon');
-
-sidebarButton.addEventListener('click', () => {
-  if (sidebar.style.left === '-250px') {
-    sidebar.style.left = '0';
-    sidebar.querySelectorAll('.sidebar-menu').forEach(menuItem => {
-      menuItem.style.display = 'block';
-    });
-    sidebar.querySelectorAll('.sidebar-icon').forEach(icon => {
-      icon.style.display = 'none';
-    });
-  } else {
-    sidebar.style.left = '-250px';
-    sidebar.querySelectorAll('.sidebar-menu').forEach(menuItem => {
-      menuItem.style.display = 'none';
-    });
-    sidebar.querySelectorAll('.sidebar-icon').forEach(icon => {
-      icon.style.display = 'block';
-    });
-  }
-});
+const toggleButton = document.querySelector('.toggleButton');
+const sidebar = document.querySelector('.sidebar');
 
 profileIcon.addEventListener('click', () => {
   alert('Profile icon clicked!');
-  // Add your profile icon click functionality here
+  // Add profile icon click functionality here
 });
+
+toggleButton.addEventListener('click', () => {
+  sidebar.classList.toggle('show');
+});    
+
