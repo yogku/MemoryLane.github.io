67d68318352543632de966152e946ced4c6e31b1
Yogesh <yogkumail@gmail.com>
2023-08-24 10:53:50 +0530

    Rename assects1 to assects

---
+++ assects/js/header.js
@@ -0,0 +1,6 @@
+const profileIcon = document.querySelector('.profileIcon');
+
+profileIcon.addEventListener('click', () => {
+  alert('Profile icon clicked!');
+  // Add profile icon click functionality here
+});
