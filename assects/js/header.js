1980f3bc15c66be7024bf17dd7c18a0d234475ea
Yogesh <yogkumail@gmail.com>
2023-10-03 14:22:07 +0530

    fixing git push

--- assects/js/header.js
+++ assects/js/header.js
@@ -4,3 +4,37 @@ profileIcon.addEventListener('click', () => {
   alert('Profile icon clicked!');
   // Add profile icon click functionality here
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const postIcon = document.getElementById('postIcon');
+  const postNav = document.querySelector('.post-nav');
+
+  let isHovering = false;
+
+  postIcon.addEventListener('mouseenter', function() {
+    isHovering = true;
+    postNav.style.opacity = '1';
+  });
+
+  postIcon.addEventListener('mouseleave', function() {
+    isHovering = false;
+    setTimeout(function() {
+      if (!isHovering) {
+        postNav.style.opacity = '0';
+      }
+    }, 200); // Delay to allow time to move mouse from icon to nav
+  });
+
+  postNav.addEventListener('mouseenter', function() {
+    isHovering = true;
+  });
+
+  postNav.addEventListener('mouseleave', function() {
+    isHovering = false;
+    setTimeout(function() {
+      if (!isHovering) {
+        postNav.style.opacity = '0';
+      }
+    }, 20); // Delay to allow time to move mouse from nav to icon
+  });
+});
