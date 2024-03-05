16540ec484ed5c9aad232d5f5de812781e4340ec
Yogesh <yogkumail@gmail.com>
2024-03-05 13:29:48 +0530

    Fix loding error

--- assects/php/post-data.php
+++ assects/php/post-data.php
@@ -8,6 +8,8 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    exit
+    
 }
 
 $result = $conn->query("SELECT * FROM posts ORDER BY id DESC");
